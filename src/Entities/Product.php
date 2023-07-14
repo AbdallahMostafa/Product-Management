6050542401c93aafa1022602b13435053039ec9d
Abdallah Mostafa <a.mokhtar@entrenchtech.com>
2023-07-14 21:09:28 +0300

    created an abstract product class to hold the common attributes that is shared by the 3 diffrent prodcuct types and added the doctrine annotation so that it can be known by the ORM that this class will have 3 subcalsses by using the DiscrminiatorMap and added setters and getters for each attribute

---
+++ src/Entities/Product.php
@@ -0,0 +1,49 @@
+<?php
+namespace Src\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+#[ORM\Entity]
+#[ORM\Table(name:"products")]
+#[ORM\InheritanceType("JOINED")]
+#[ORM\DiscriminatorColumn(name: "type", type:"string")]
+#[ORM\DiscriminatorMap([
+      "Book" => BookProduct::class,
+      "DVD" => DVDProduct::class,
+      "Furniture" => FurnitureProduct::class
+ ])]
+abstract class Product {
+    
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+    #[ORM\Column(type: 'string')]
+    protected string $SKU;
+    #[ORM\Column(type: 'string')]
+    protected string $name;
+    #[ORM\Column(type: 'integer')]
+    protected int $price;
+
+    public function getName(): string {
+        return $this->name;
+    }
+    public function setName($name): void
+    {
+        $this->name =$name;
+    }
+    public function getSKU(): string {
+        return $this->SKU;
+    }
+    public function setSKU($SKU): void {
+        $this->SKU = $SKU;
+    }
+    public function getPrice(): int {
+        return $this->price;
+    }
+    public function setPrice($price): void {
+        $this->price = $price;
+    }
+}
