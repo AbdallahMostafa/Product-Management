8b80fb833c75f05c725371cb5b6c3f4d62bb76b9
Abdallah Mostafa <a.mokhtar@entrenchtech.com>
2023-07-14 20:09:08 +0300

    creating the Src directory and writting the register function logic by passing the  class name as an argument to construct the file path by replacing  the namespace seperator with the directory seperator and appending the '.php' extension and the file_exsists checks if the file exsisits if so then require it once

---
+++ Autoloader/src/Autoloader.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace CustomAutoloaderPackage;
+
+class Autoloader  
+{
+    public static function register()
+    {
+        spl_autoload_register(function ($class) {
+            $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+            if (file_exists($file)) {
+                require_once $file;
+            }
+        });
+    }
+}
