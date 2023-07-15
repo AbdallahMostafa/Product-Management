89c6ff8166dc99a73871c81804496607854c440e
Abdallah Mostafa <a.mokhtar@entrenchtech.com>
2023-07-15 06:01:58 +0300

    created the Product factory to create the object based on the product type

---
+++ src/Factory/Product/ProductFactroy.php
@@ -0,0 +1,30 @@
+<?php
+namespace Src\Factory\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Src\Entities\BookProduct;
+use Src\Entities\DVDProduct;
+use Src\Entities\FurnitureProduct;
+use Src\Entities\Product;
+
+class ProductFactroy {
+    protected $product;
+    
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+    public function createProduct(string $type) {
+        if ($type === 'Book') {
+            $product = new BookProduct();
+        } elseif ($type === 'DVD') {
+            $product = new DVDProduct();
+        } elseif ($type === 'Furniture') {
+            $product = new FurnitureProduct();
+        } else {
+            throw new InvalidArgumentException("Invalid product type: $type");
+        }
+        return $product;
+    }
+}
