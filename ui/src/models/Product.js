200fb94b1e1e7c6094832c06f661b51854bcfa4d
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-07-22 00:33:51 +0300

    product model

---
+++ ui/src/models/Product.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(name, price, attribute) {
+      this.name = name;
+      this.price = price;
+      this.attribute = attribute;
+    }
+  }
+  
+  export default Product;
