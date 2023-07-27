3d2e8347f4f2ef52bc01e693f36de4631e82aa17
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-07-27 03:32:22 +0300

    adding the logic to check for the preflight request and add the cors headers

--- src/Controllers/helpers/response.php
+++ src/Controllers/helpers/response.php
@@ -1,7 +1,24 @@
 <?php
 
+function setCorsHeaders() {
+    header('Access-Control-Allow-Origin: http://localhost:3000'); 
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    // header('Access-Control-Allow-Credentials: true'); // Add this if you need to include credentials
+}
+
+// Check if the request is a preflight request (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    setCorsHeaders();
+    exit; // Respond to the preflight request and stop further execution
+}
+
+// Add CORS headers for other requests
+setCorsHeaders();
+
+// Function to send a JSON response
 function jsonResponse($data, $statusCode = 200) {
-    // header('Content-Type: application/json');
+    header('Content-Type: application/json');
     http_response_code($statusCode);
     echo json_encode($data);
     exit;
