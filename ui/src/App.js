4e36600da73b7ab66530a71c785bfe51aae680a5
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-07-27 03:36:57 +0300

    calling  the products components

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ProductList from './components/ProductList';
+import AddProductForm from './components/AddProductForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product App</h1>
+      <ProductList />
+      <AddProductForm />
     </div>
   );
 }
