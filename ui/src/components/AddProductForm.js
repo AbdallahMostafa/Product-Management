28d3884901ba936d04ba0a536b786f1b24165a62
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-08-09 19:58:14 +0300

    removing the useState and wireing the component to the redux store, by dispatching the addProduct action in the handleSubmit, and changing handle attriubtes to dispatch setFormFiled action to handle the from fileds

--- ui/src/components/AddProductForm.js
+++ ui/src/components/AddProductForm.js
@@ -1,115 +1,108 @@
-import React, {useState} from 'react';
-import productService from '../services/productService';
-import Product from '../models/Product';
-
-const AddProductForm = () => {
+    import React from 'react';
+    import { useNavigate, useLocation } from 'react-router-dom';
     
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [SKU, setSKU] = useState('');
-    const [type, setType] = useState('');
-
-    const [attributes, setAttributes] = useState([]);
-    
-    const attributeMap = {
-        DVD: { size: 'size' },
-        Book: { weight: 'weight' },
-        Furniture: { weight: 'weight', length: 'length', height: 'height' },
-    };
-
-    const handleSKUChange =(event) => {
-        setSKU(event.target.value);
-    }
-
-    const handleTypeChange =(event) => {
-        setType(event.target.value);
-    }
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
-    
-    const handlePriceChange = (event) => {
-        setPrice(event.target.value);
-    };
-    
-    const handleAttributeChange = (event) => {
-        const attributeName = event.target.name;
-        const attributeValue = event.target.value;
-
-        // Get the attribute name based on the selected type
-        const attributeType = attributeMap[type];
-        if (!attributeType) {
-            console.error('Invalid product type:', type);
-            return;
-        }
+    import Product from '../models/Product';
+    import DVDProductForm from '../productForms/DVDProductForm';
+    import BookProductForm from '../productForms/BookProductForm';
+    import FurnitureProductForm from '../productForms/FurnitureProductForm';
+    import '../styles/form.css';
+    import { connect } from 'react-redux';
+    import {Button} from 'react-bootstrap';
+    import { addProduct, setFormField } from '../store/action'; // Import the relevant actions
 
 
-        setAttributes({ ...attributes, [attributeType[attributeName]]: attributeValue });
-    };
 
-    const handleSubmit = async (event) => {
+    const AddProductForm = ({ formState, addProduct, setFormField }) => {
         
-        event.preventDefault();
+        const { name, price, SKU, type, attributes } = formState;
 
-        const product = new Product(name, price, attributes, SKU, type);
         
-        console.log(product);
-        try {
-            await productService.addProduct(product);
-            setName('');
-            setPrice('');
-            setAttributes('');
-            alert('Product added successfully!');
-        } catch (error) {
-            console.error('Error adding product:', error);
-            alert('Falied to add product. Please try again.');
-        }
-    };
+        const navigate = useNavigate();
 
-    const typeSpecificFields = {
-        DVD: (
-          <div>
-            <label htmlFor="size">Size (MB):</label>
-            <input type="number" id="size" name="size" onChange={handleAttributeChange}/>
-          </div>
-        ),
-        Book: (
-          <div>
-            <label htmlFor="weight">Weight (Kg):</label>
-            <input type="number" id="weight" name="weight" onChange={handleAttributeChange}/>
-          </div>
-        ),
-        Furniture: (
-          <div>
-            <label htmlFor="dimensions">Dimensions (HxWxL):</label>
-            <input type="text" id="dimensions" name="dimensions" onChange={handleAttributeChange} />
-          </div>
-        )
-    };
+
+        const handleFieldChange = (event) => {
+            const { name, value } = event.target;
+            setFormField(name, value);
+        };
     
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" onChange={handleNameChange}/>
+        const handleAttributeChange = (attributeName, attributeValue) => {
+            const newAttributes = {
+              ...attributes,
+              [attributeName]: attributeValue,
+            };
+            setFormField('attributes', newAttributes); // This dispatches the action for the attributes object
+          };
+        const handleSubmit = async (event) => {
+                event.preventDefault();
+                
 
-            <label htmlFor="price">Price:</label>
-            <input type="number" id="price" name="price" onChange={handlePriceChange}/>
 
-            <label htmlFor="SKU">SKU:</label>
-            <input type="text" id="SKU" name="SKU" onChange={handleSKUChange}/>
+                const product = new Product(name, price, attributes, SKU, type);
+                
+                console.log(product);
+                try {
+                    addProduct(product);
+                    alert('Product added successfully!');
+                    navigate('/');
+                } catch (error) {
+                    console.error('Error adding product:', error);
 
-            <label htmlFor="type">Type:</label>
-            <select id="type" value={type} onChange={handleTypeChange}>
-                <option value="">Select a type</option>
-                <option value="DVD">DVD</option>
-                <option value="Book">Book</option>
-                <option value="Furniture">Furniture</option>
-            </select>
-            {typeSpecificFields[type]}
+                    alert('Falied to add product. Please try again.');
+                }
+                
+            
+        };
 
-            <button type="submit">Add Product</button>
-        </form>
-    );
+        const typeSpecificFields = {
+            DVD: <DVDProductForm attributes={attributes} handleAttributeChange={handleAttributeChange} />,
+            Book: <BookProductForm attributes={attributes} handleAttributeChange={handleAttributeChange} />,
+            Furniture: <FurnitureProductForm  attributes={attributes} handleAttributeChange={handleAttributeChange} />,
+        };
+        
+        return (
+            <div>
+                <header className='form-header'>
+                    <div>
+                        <h2>Product Add</h2>
+                    </div>
+                    <Button onClick={handleSubmit} variant="success" >Save</Button>
+                </header>
+                <hr></hr>
+                <form className="form-container">
+                    
+                    <label htmlFor="name" className="label-style">Name:</label>
+                    <input className="input-style" type="text" id="name"  value={name} name="name" onChange={handleFieldChange}/>
+
+                    <label htmlFor="price" className='label-style'>Price:</label>
+                    <input className='input-style'type="number" id="price" name="price" value={price} onChange={handleFieldChange}/>
+
+                    <label htmlFor="SKU" className='label-style'>SKU:</label>
+                    <input className='input-style' type="text" id="SKU" value={SKU} name="SKU" onChange={handleFieldChange}/>
+
+                    <label htmlFor="type" className='label-style'>Type:</label>
+                    <select className="select-style" id="type" value={type} name='type' onChange={handleFieldChange}>
+                        <option value="">Select a type</option>
+                        <option value="DVD">DVD</option>
+                        <option value="Book">Book</option>
+                        <option value="Furniture">Furniture</option>
+                    </select>
+                    {typeSpecificFields[type]}
+
+                    <button className='button-style' type="button" onClick={() => navigate('/')}>
+                        Cancel
+                    </button>    
+                </form>
+            </div>
+        );
+    };
+
+const mapStateToProps = (state) => ({
+    formState: state.addProductForm, // Use the correct reducer name
+});
+    
+const mapDispatchToProps = {
+    addProduct,
+    setFormField,
 };
-
-export default AddProductForm;
+      
+export default connect(mapStateToProps, mapDispatchToProps)(AddProductForm);
