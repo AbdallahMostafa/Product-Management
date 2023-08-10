530df2cda906482c779e95699ad32f0168674032
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-08-10 14:46:27 +0300

    adding route to the delete function

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -6,5 +6,5 @@ $productController = $container->get(ProductController::class);
 return [
     ['GET', '/api/products', [$productController, 'index']],
     ['POST', '/api/products', [$productController, 'store']],
-    // Additional routes...
+    ['DELETE', '/api/products', [$productController, 'delete']],
 ];
