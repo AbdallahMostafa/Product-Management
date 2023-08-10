e8ef118d99bcf6e7219a9f979892b39b1ad90be6
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-08-10 20:30:53 +0300

    adding PHPDocs, and formating the file

--- Autoloader/src/Autoloader.php
+++ Autoloader/src/Autoloader.php
@@ -3,12 +3,29 @@
 
 namespace CustomAutoloaderPackage;
 
-class Autoloader  
+/**
+ * Class Autoloader
+ *
+ * A simple autoloader implementation to load classes based on namespaces.
+ */
+class Autoloader
 {
+    /**
+     * Register the autoloader with the SPL autoloader stack.
+     *
+     * This method sets up the autoloader to load classes automatically based on their namespaces.
+     * It uses a closure to include the corresponding class file if it exists.
+     *
+     * @return void
+     */
     public static function register()
     {
         spl_autoload_register(function ($class) {
+            // Convert the namespace to a file path
+
             $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+            // Check if the file exists and load it
+
             if (file_exists($file)) {
                 require_once $file;
             }
