9b1b36bceeab5c7904f67342641012e47ead0720
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-08-10 20:22:40 +0300

    adding PHPDoc and formating the file

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -6,15 +6,37 @@ use Doctrine\ORM\EntityManagerInterface;
 require_once __DIR__ .'/helpers/response.php';
 use Src\Factory\Product\ProductFactroy;
 use Src\Entities\Product;
-class ProductController {
 
+/**
+ * ProductController handles product operations.
+ */
+class ProductController
+{
+
+    /**
+     * @var EntityManagerInterface The entity manager instance.
+     */
     private $entityManager;
+    /**
+     * @var ProductFactroy The product factory instance.
+     */
     private $factory;
+
+     /**
+     * ProductController constructor.
+     *
+     * @param EntityManagerInterface $entityManager The entity manager instance.
+     * @param ProductFactroy $factory The product factory instance.
+     */
     public function __construct(EntityManagerInterface $entityManager, ProductFactroy $factory)
     {
         $this->factory = $factory;
         $this->entityManager = $entityManager;
     }
+
+    /**
+     * Fetches and responds with a list of products.
+     */
     public function index()
     {
         $productRepository = $this->entityManager->getRepository(Product::class);
@@ -39,17 +61,29 @@ class ProductController {
         // Respond with the fetched product data
         jsonResponse($productData, 200);
     }
+    /**
+     * Creates and stores a new product.
+     *
+     * @param array $request The product data from the request.
+     */
     public function store($request)
-    {   
-        $product = $this->factory->createProduct($request['type'],$request);
+    {
+        $product = $this->factory->createProduct($request['type'], $request);
         
         $this->entityManager->persist($product);
         $this->entityManager->flush();
         jsonResponse([
             'message' => 'Product created successfully',
-            'request' => $product], 201);   
+            'request' => $product], 201);
     }
-    public function delete($request) {
+
+    /**
+     * Deletes selected products.
+     *
+     * @param array $request The request data containing product IDs.
+     */
+    public function delete($request)
+    {
         
         $requestData = json_decode(file_get_contents('php://input'), true);
 
--- src/Controllers/helpers/response.php
+++ src/Controllers/helpers/response.php
@@ -1,7 +1,11 @@
 <?php
 
-function setCorsHeaders() {
-    header('Access-Control-Allow-Origin: http://localhost:3000'); 
+/**
+ * Set CORS headers to allow cross-origin requests.
+ */
+function setCorsHeaders()
+{
+    header('Access-Control-Allow-Origin: http://localhost:3000');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
     // header('Access-Control-Allow-Credentials: true'); // Add this if you need to include credentials
@@ -16,8 +20,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 // Add CORS headers for other requests
 setCorsHeaders();
 
-// Function to send a JSON response
-function jsonResponse($data, $statusCode = 200) {
+
+/**
+ * Send a JSON response with the specified data and status code.
+ *
+ * @param mixed $data The data to be sent as JSON.
+ * @param int $statusCode The HTTP status code.
+ * @return void
+ */
+function jsonResponse($data, $statusCode = 200)
+{
     header('Content-Type: application/json');
     http_response_code($statusCode);
     echo json_encode($data);
