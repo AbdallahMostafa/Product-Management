a36183f373a9529cd05de155ca8eeb2c95895892
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-08-12 20:04:53 +0300

    removing the product context

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import ProductList from './components/ProductList';
 import AddProductForm from './components/AddProductForm';
-import { ProductProvider } from './context/ProductContext';
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>      
-      <ProductProvider>
       <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<ProductList/>} />
             <Route exact path="/add-product" element={<AddProductForm  />} />
           </Routes>
         </BrowserRouter>
-      </ProductProvider>
 
     </div>
   );
