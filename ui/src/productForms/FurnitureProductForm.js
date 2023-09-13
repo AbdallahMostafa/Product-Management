fbb3718bcc6e8c036f88510ff7817861d1d64ade
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-09-13 05:30:45 +0300

    changed the weight -> width, added a placeholder for each  input, and removed the unused attributes

--- ui/src/productForms/FurnitureProductForm.js
+++ ui/src/productForms/FurnitureProductForm.js
@@ -2,27 +2,27 @@ import React from 'react';
 import '../styles/furnitureForm.css';
 import ProductFormWithTooltips from '../components/ProductFormWithTooltips';
 
-const FurnitureProductForm = ({ attributes, handleAttributeChange }) => {
+const FurnitureProductForm = ({ handleAttributeChange,  }) => {
   return (
     <div>
       <div>
-        <label htmlFor="weight">Weight:</label>
-        <ProductFormWithTooltips title="Enter weight in KG">
-          <input  className="input-style" type="number" id="weight" name="weight" onChange={(e) => handleAttributeChange('weight', e.target.value)} />
+        <label htmlFor="width">Width:</label>
+        <ProductFormWithTooltips title="Enter width in KG">
+          <input  className="input-style" type="number" id="width" name="width" placeholder="Enter Width" onChange={(e) => handleAttributeChange('width', e.target.value)} />
         </ProductFormWithTooltips>
 
       </div>
       <div>
         <label htmlFor="height">Height:</label>
         <ProductFormWithTooltips title="Enter height in cm" className="input-style" >
-          <input className="input-style" type="number" id="height" name="height"  onChange={(e) => handleAttributeChange('height', e.target.value)} />
+          <input className="input-style" type="number" id="height" name="height" placeholder="Enter Height"  onChange={(e) => handleAttributeChange('height', e.target.value)} />
         </ProductFormWithTooltips>
 
       </div>
       <div>
         <label htmlFor="length">Length:</label>
         <ProductFormWithTooltips title="Enter length in cm">
-          <input className="input-style" type="number" id="length" name="length"  onChange={(e) => handleAttributeChange('length', e.target.value)} />
+          <input className="input-style" type="number" id="length" name="length" placeholder="Enter Length" onChange={(e) => handleAttributeChange('length', e.target.value)} />
         </ProductFormWithTooltips>
 
       </div>
